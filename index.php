4234f3c0237cf4451fb385c1883e9328a3c764c2
delta123pangrib <deltaflorentina123@gmail.com>
2020-02-24 09:38:50 +0700

    Add any words

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
     echo "Hello World";
     echo " Ayok Kerjakan TA-mu";
-    echo '<p>   delta </p>';
+    echo "<p>   delta </p>";
+    echo "<h2>PHP is Fun!</h2>";
+    echo "Hello world!<br>";
+    echo "I'm about to learn PHP!<br>";
+    echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 ?>
 <!DOCTYPE html>
 <html lang="en">
